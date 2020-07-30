18fafce81837efbf8bf5deb0a5aafbe9b0c6fbe1
JessicaGilfillan <49848402+JessicaGilfillan@users.noreply.github.com>
2020-07-29 20:34:15 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+/* 
+
+Using the form displayed in index.html, create the following functionality using JavaScript. 
+
+If the user tries to submit the form without entering any info, display an error message on the page that tells them all info is required. 
+When users submit their first name and favourite colour, display a customized welcome message on the page in a new H1 that includes their name. Change the background colour of the page to their favourite colour. 
+
+*/
